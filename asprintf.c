888a5dd3683f85b7b91b8f8de6657323cfc91d65
Thomas Gamper <icicle@cg.tuwien.ac.at>
2018-03-17 12:49:12 +0100

    TG: - asprintf.c convert tabs to spaces

--- asprintf.c
+++ asprintf.c
@@ -9,45 +9,45 @@ extern "C"
 #endif
 int vasprintf(char** strp, const char* fmt, va_list ap)
 {
-	int formattedLength, actualLength;
-	size_t requiredSize;
+    int formattedLength, actualLength;
+    size_t requiredSize;
 
-	// be paranoid
-	*strp = NULL;
+    // be paranoid
+    *strp = NULL;
 
-	// compute length of formatted string, without NULL terminator
-	formattedLength = _vscprintf(fmt, ap);
+    // compute length of formatted string, without NULL terminator
+    formattedLength = _vscprintf(fmt, ap);
 
-	// bail out on error
-	if (formattedLength < 0)
-	{
-		return -1;
-	}
+    // bail out on error
+    if (formattedLength < 0)
+    {
+        return -1;
+    }
 
-	// allocate buffer, with NULL terminator
-	requiredSize = ((size_t)formattedLength) + 1;
-	*strp = (char*)malloc(requiredSize);
+    // allocate buffer, with NULL terminator
+    requiredSize = ((size_t)formattedLength) + 1;
+    *strp = (char*)malloc(requiredSize);
 
-	// bail out on failed memory allocation
-	if (*strp == NULL)
-	{
-		errno = ENOMEM;
-		return -1;
-	}
+    // bail out on failed memory allocation
+    if (*strp == NULL)
+    {
+        errno = ENOMEM;
+        return -1;
+    }
 
-	// write formatted string to buffer, use security hardened _s function
-	actualLength = vsnprintf_s(*strp, requiredSize, requiredSize - 1, fmt, ap);
+    // write formatted string to buffer, use security hardened _s function
+    actualLength = vsnprintf_s(*strp, requiredSize, requiredSize - 1, fmt, ap);
 
-	// again, be paranoid
-	if (actualLength != formattedLength)
-	{
-		free(*strp);
-		*strp = NULL;
-		errno = EOTHER;
-		return -1;
-	}
+    // again, be paranoid
+    if (actualLength != formattedLength)
+    {
+        free(*strp);
+        *strp = NULL;
+        errno = EOTHER;
+        return -1;
+    }
 
-	return formattedLength;
+    return formattedLength;
 }
 
 #ifdef __cplusplus
@@ -55,13 +55,13 @@ extern "C"
 #endif
 int asprintf(char** strp, const char* fmt, ...)
 {
-	int result;
+    int result;
 
-	va_list ap;
-	va_start(ap, fmt);
-	result = vasprintf(strp, fmt, ap);
-	va_end(ap);
+    va_list ap;
+    va_start(ap, fmt);
+    result = vasprintf(strp, fmt, ap);
+    va_end(ap);
 
-	return result;
+    return result;
 }
 #endif
